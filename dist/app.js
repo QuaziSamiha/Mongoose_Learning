608614deaf4a0290896c9a080b98fa2826199141
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-06-14 19:36:56 +0600

    project set up part 2

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// 14 June, 2024
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+console.log(process.cwd());
+// export default app;
