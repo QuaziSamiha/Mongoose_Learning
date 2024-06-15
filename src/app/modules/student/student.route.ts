6e677d84afb328e10504484b6241b5ea30a22e59
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-06-15 22:29:25 +0600

    insert a data to database, used postman

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import { StudentContollers } from './student.controller';
 
 // route will call controller function
+// router is an object
 const router = express.Router();
 
 // will call controller function
 router.post('/create-student', StudentContollers.createStudent);
+
+export const StudentRoutes = router;
