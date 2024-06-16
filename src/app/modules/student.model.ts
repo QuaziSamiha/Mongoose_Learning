9f5c881ca0a801fb36b86cc26d37599d92f09b83
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-06-16 17:09:18 +0600

    validation done using Validator library part 1

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -1,4 +1,4 @@
-// 18 , 21 May, 2024
+// 18, 21 May, 2024
 // 15, 16 June, 2024
 import { Schema, model } from 'mongoose';
 import {
@@ -7,29 +7,24 @@ import {
   Student,
   UserName,
 } from './student/student.interface';
+import validator from 'validator';
 
 const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
-    // built in validation of mongoose
-    required: [true, 'First name is required'], // custom message
-    trim: true, // remove extra spaces
-    maxlength: [20, 'Max allowed length is 20'], // custom message (optinal)
-    validate: {
-      validator: function (value: string) {
-        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
-        return firstNameStr === value;
-      },
-      message: '{VALUE} is not in capitalize format',
-    },
+    required: true,
   },
   middleName: {
     type: String,
   },
   lastName: {
     type: String,
-    // built in validation of mongoose
-    required: [true, 'Last name is required'], // custom message
+    required: true,
+    // USING VALIDATOR LIBRARY
+    validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: '{VALUE} is not valid',
+    },
   },
 });
 
@@ -50,51 +45,46 @@ const localGaurdianSchema = new Schema<LocalGaurdian>({
 });
 
 const studentSchema = new Schema<Student>({
-  id: { type: String, required: true, unique: true }, // built in validation of mongoose
-  // built in validation of mongoose
+  id: { type: String, required: true },
   name: {
     type: userNameSchema,
-    required: [true, 'Name is required samiha babu'], // custom message
+    required: true,
   },
-  // enum (built in validation of mongoose)
+  // built in validation of mongoose
   gender: {
     type: String,
-    enum: {
-      values: ['male', 'female', 'other'],
-      message: "Gender only can be 'male' or 'female' or 'other'", // adding custom message
-    },
+    enum: ['male', 'female', 'other'],
     required: true,
   },
   dateOfBirth: { type: String },
-  email: { type: String, required: true, unique: true }, // built in validation of mongoose
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value: string) => validator.isEmail(value),
+      message: '{VALUE} is not a valid email type',
+    },
+  },
   contactNo: { type: String, required: true },
   emergencyContactNo: { type: String, required: true },
-  // built in validation of mongoose (enum)
   bloodGroup: {
     type: String,
-    enum: {
-      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
-      message: '{VALUE} is not valid', // 'VALUE' gives the value tried to insert
-    },
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
   },
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  // built in validation of mongoose
   gaurdian: {
     type: gaurdianSchema,
     required: true,
   },
-  // built in validation of mongoose
   localGaurdian: {
     type: localGaurdianSchema,
     required: true,
   },
   profileImg: { type: String },
-  // built in validation of mongoose (enum)
   isActive: {
     type: String,
     enum: ['active', 'blocked'],
-    default: 'active',
   },
 });
 
